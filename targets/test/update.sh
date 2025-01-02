959cb01001bd5f466675ade1736243d68da38501
Your Name <you@example.com>
2025-01-02 15:08:06 +0000

    wifi:upd

--- targets/test/update.sh
+++ targets/test/update.sh
@@ -2,14 +2,15 @@
 
 URL="https://github.com/routerich/packages.routerich/raw/refs/heads/test_3012/targets/test"
 DIR="/lib/modules/5.15.167"
-modules="cfg80211.ko
+modules="batman-adv.ko
+cfg80211.ko
 mac80211.ko
 mt76-connac-lib.ko
 mt76.ko
 mt7915e.ko"
 
-grep -q 369cd3b56ad9cf95bd112cd6fe72ef5b /usr/lib/opkg/info/kernel.control || { 
-  echo "Kernel missmatch!" 
+grep -q 369cd3b56ad9cf95bd112cd6fe72ef5b /usr/lib/opkg/info/kernel.control || {
+  echo "Kernel missmatch!"
   exit 1
 }
 
@@ -17,6 +18,9 @@ for module in $modules
 do
   wget -O "$DIR/$module" "$URL/$module"
 done
+
+wget -O "/usr/sbin/wpad" "$URL/wpad"
+chmod +x /usr/sbin/wpad
 
 echo wed off
 echo mt7915e > /etc/modules.d/mt7915e
